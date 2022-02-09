49d9bf3b3e59733faf7128a5f80e23497301f553
Artem Anurin <anurin93@gmail.com>
2022-02-09 16:44:31 +0300

    написано дз1 на java

---
+++ HomeWork1.java
@@ -0,0 +1,48 @@
+public class HomeWork1 {
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    public static void checkSumSign() {
+        int a = 28;
+        int b = -33;
+
+        if (a+b >= 0) {
+            System.out.println("The sum is positive");
+        }
+        if (a+b <= 0) {
+            System.out.println("The sum is negative");
+        }
+
+    }
+
+    public static void printColor() {
+        int value = 8;
+
+        if (value <= 0) {
+            System.out.println("Red");
+        } else if (value <=100) {
+            System.out.println("Yellow");
+        } else if (value >100){
+            System.out.println("Green");
+        }
+    }
+    public static void compareNumbers() {
+        int a = 75;
+        int b = 59;
+        if (a>=b) {
+            System.out.println("a>=b");
+        } else if (a<b) {
+            System.out.println("a<b");
+        }
+    }
+}
